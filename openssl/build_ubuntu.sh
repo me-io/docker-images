b34eedbaca81f14196f46de827c28401484193d3
Mohamed Meabed <mo.meabed@gmail.com>
2018-08-21 01:57:25 +0400

    Init

---
+++ openssl/build_ubuntu.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+# https://hub.docker.com/_/ubuntu/
+IMG_NAME="ubuntu"
+IMG_TAG="16.04"
+# https://www.openssl.org/source/
+OPENSSL_VER="openssl-1.1.0i"
+OPENSSL_PREFIX="/usr/local/ssl"
+# docker hub image_name:tag
+REPO_NAME="meio/ubuntu"
+REPO_TAG=${OPENSSL_VER}
+
+if [[ ! -z "${DOCKER_PASSWORD}" && ! -z "${DOCKER_USERNAME}" ]]
+then
+    echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+fi
+
+TAG_EXIST=`curl -s "https://hub.docker.com/v2/repositories/${REPO_NAME}/tags/${REPO_TAG}/" | grep '"id":'`
+
+if [[ ! -z ${TAG_EXIST}  ]]; then
+    echo "${REPO_NAME}:${REPO_TAG} already exist"
+    exit 0
+fi
+
+docker build --build-arg IMG_NAME=${IMG_NAME} \
+             --build-arg IMG_TAG=${IMG_TAG} \
+             --build-arg OPENSSL_VER=${OPENSSL_VER} \
+             --build-arg OPENSSL_PREFIX=${OPENSSL_PREFIX} \
+             -t ${REPO_NAME}:${REPO_TAG} ${DIR}
+
+if [[ $? != 0 ]]; then
+    echo "${REPO_NAME}:${REPO_TAG} build failed"
+    exit 1
+fi
+
+
+if [[ -z ${TAG_EXIST}  ]]; then
+    docker push ${REPO_NAME}:${REPO_TAG}
+    echo "${REPO_NAME}:${REPO_TAG} pushed successfully"
+fi
