700bd131a998766e23a16ef02be3a79c7862ddfd
Mohamed Meabed <mo.meabed@gmail.com>
2018-10-22 15:06:39 +0400

    Update pm2 images

---
+++ pm2/build_alpine.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+# https://hub.docker.com/_/ubuntu/
+IMG_NAME="node"
+IMG_TAG="10.12.0-alpine"
+MOZ_TAG="v3.3.1"
+
+# docker hub image_name:tag
+REPO_NAME="meio/pm2"
+REPO_TAG=${IMG_TAG}
+
+if [[ ! -z "${DOCKER_PASSWORD}" && ! -z "${DOCKER_USERNAME}" ]]
+then
+    echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+fi
+
+TAG_EXIST=`curl -s "https://hub.docker.com/v2/repositories/${REPO_NAME}/tags/${REPO_TAG}/" | grep '"id":'`
+
+if [[ ! -z ${TAG_EXIST}  ]]; then
+    echo "${REPO_NAME}:${REPO_TAG} already exist"
+    exit 0
+fi
+
+docker build --build-arg IMG_NAME=${IMG_NAME} \
+             --build-arg IMG_TAG=${IMG_TAG} \
+             --build-arg MOZ_TAG=${MOZ_TAG} \
+             -t ${REPO_NAME}:${REPO_TAG} ${DIR}
+
+if [[ $? != 0 ]]; then
+    echo "${REPO_NAME}:${REPO_TAG} build failed"
+    exit 1
+fi
+
+
+if [[ -z ${TAG_EXIST}  ]]; then
+    docker push ${REPO_NAME}:${REPO_TAG}
+    echo "${REPO_NAME}:${REPO_TAG} pushed successfully"
+fi
